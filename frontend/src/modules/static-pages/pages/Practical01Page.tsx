a77af33f18738e5ec45798390b2dd1c2bd6e0031
Patrik Pukan <patrik.pukan@gmail.com>
2025-09-21 17:58:27 +0200

    feat: Add counter functionality to Practical01Page

--- frontend/src/modules/static-pages/pages/Practical01Page.tsx
+++ frontend/src/modules/static-pages/pages/Practical01Page.tsx
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import { Box, Button } from '@chakra-ui/react';
 
 import { Heading, Paragraph } from '@frontend/shared/design-system/components';
 
 export function Practical01Page() {
+  const [counter, setCounter] = useState(0);
   return (
     <Box>
       <Heading>Practical 01</Heading>
-      <Paragraph>Hello!</Paragraph>
-      <Button onClick={() => alert('Button Pressed!')}>Press Me</Button>
+      <Paragraph>Counter: {counter}</Paragraph>
+      <Button onClick={() => setCounter((c) => c + 1)}>+1</Button>
     </Box>
   );
 }
